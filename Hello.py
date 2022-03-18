21341f0b77fedcabf172c9700fd5df8340f7f109
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2022-03-18 17:11:49 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,9 +2,9 @@
 Touched by user2
 test str 안녕하세요 후후후
 
-x = int(input())
+
 print("Hello everyone")
-print("hello everyone") 
+ 
 print("김득호 202004110,컴퓨터.전자시스템공학과")
 print("과제야아아아...")
 # 너무 많은 수정요청으로 곤란합니다^ㅡ^
